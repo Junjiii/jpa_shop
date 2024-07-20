8dcdccbfa792bdc5419389898743284b8c11801d
Jjunjiii’ <drum1701@gmail.com>
2024-07-20 15:13:14 +0900

    JPA Entity

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtpye")
+@Getter
+@Setter
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int stockQuantity;
+
+    private int price;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+
+}
